6ac49f968abce888755ca35f2c53e20bfd4cf164
efdgdfgdf <2571599538@qq.com>
2023-02-27 10:45:31 +0800

    first add files
    first add files

---
+++ config.js
@@ -0,0 +1,38 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '站点监控🔋',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2033713-d213a788f105c1901858c287',
+    'ur2033713-23436abc547bdf7cf8b6e08a',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://home.dp2002.top/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/efdgdfgdf/'
+    },
+    {
+      text: 'Blog',
+      url: 'https://www.dp2002.top/'
+    },
+    {
+      text: 'Music',
+      url: 'https://music.dp2002.top/'
+    },
+  ],
+};
